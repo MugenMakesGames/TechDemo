fb01fc5849415cf4f439c396063cd602add9d0c0
Mugen <52742495+eyimofe-olasoko@users.noreply.github.com>
2026-05-18 21:24:47 +0100

    Initial Commit

---
+++ Source/TechDemo/TechDemo.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TechDemo : ModuleRules
+{
+	public TechDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"TechDemo",
+			"TechDemo/Variant_Horror",
+			"TechDemo/Variant_Horror/UI",
+			"TechDemo/Variant_Shooter",
+			"TechDemo/Variant_Shooter/AI",
+			"TechDemo/Variant_Shooter/UI",
+			"TechDemo/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
